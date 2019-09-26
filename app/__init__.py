bcd11faf9ad993fac22fd131e2618c5528466add
ziedbouf <zied.boufaden@gmail.com>
2019-09-26 14:25:04 +0300

    add resource endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
-from flask_restplus import Api
-from flask import Blueprint
+import os
+from flask import Blueprint, request, jsonify
+from prometheus_client import CONTENT_TYPE_LATEST, generate_latest
+from flask_restplus import Api, Namespace, Resource
 
 from .main.controller.user_controller import api as user_ns
 from .main.controller.auth_controller import api as auth_ns
+from .main import metrics
+
 
 blueprint = Blueprint('api', __name__)
 
@@ -12,5 +16,29 @@ api = Api(blueprint,
           description='a boilerplate for flask restplus web service'
           )
 
+metrics_ns = Namespace('metrics', description='prometheus metrics collector endpoints')
+
+@metrics.do_not_track()
+@metrics_ns.route('/')
+class PrometheusMetricsEndpoint(Resource):
+
+    def get(self):
+      from prometheus_client import multiprocess, CollectorRegistry
+
+      if 'prometheus_multiproc_dir' in os.environ:
+          registry = CollectorRegistry()
+      else:
+          registry = metrics.registry
+
+      if 'name[]' in request.args:
+          registry = registry.restricted_registry(request.args.getlist('name[]'))
+
+      if 'prometheus_multiproc_dir' in os.environ:
+          multiprocess.MultiProcessCollector(registry)
+  
+      headers = {'Content-Type': CONTENT_TYPE_LATEST}
+      return generate_latest(metrics.registry), 200, headers
+
+api.add_namespace(metrics_ns, path='/metrics')
 api.add_namespace(user_ns, path='/user')
 api.add_namespace(auth_ns)
--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_reverse_proxy_fix.middleware import ReverseProxyPrefixFix
+from prometheus_flask_exporter import PrometheusMetrics
+
 
 from .config import config_by_name
 
 db = SQLAlchemy()
 flask_bcrypt = Bcrypt()
+metrics = PrometheusMetrics(None)
 
 
 def create_app(config_name):
@@ -14,7 +17,8 @@ def create_app(config_name):
     app.config.from_object(config_by_name[config_name])
     app.config['REVERSE_PROXY_PATH'] = '/api'
     ReverseProxyPrefixFix(app)
-    db.init_app(app)
+    db.init_app(app=app)
+    metrics.init_app(app)
     flask_bcrypt.init_app(app)
 
     return app
